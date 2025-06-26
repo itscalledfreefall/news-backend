ecaeb0ebe97b0ef79913b8c39e1da3d0f122ca6b
ata demir alçınar <demirata07@hotmail.com>
2025-06-26 17:29:07 +0300

    post mapping deleted cuz it was not useful

--- src/main/java/com/itscalledfreefall/news/local_news_backend/Controllers/ArticleController.java
+++ src/main/java/com/itscalledfreefall/news/local_news_backend/Controllers/ArticleController.java
@@ -1,10 +1,9 @@
 package com.itscalledfreefall.news.local_news_backend.Controllers;
 
-import com.itscalledfreefall.news.local_news_backend.exceptions.ArticleNotFoundEx;
+
 import com.itscalledfreefall.news.local_news_backend.model.Article;
 import com.itscalledfreefall.news.local_news_backend.services.ArticleService;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,19 +27,10 @@ public class ArticleController {
     }
     @GetMapping("/{id}")
     public ResponseEntity<Article> getArticleById(@PathVariable Long id){
-        try{
+
         Optional<Article> article = service.getArticleById(id);
         return article.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());}
 
-        catch (ArticleNotFoundEx ex ){
-            return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-    @PostMapping
-    public ResponseEntity<Article> createArticle(@RequestBody Article article){
-        Article savedArticle = service.saveArticle(article);
-        return ResponseEntity.ok(savedArticle);
-    }
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteByIdArticle(@PathVariable Long id ){
         service.deleteByIdArticle(id);
